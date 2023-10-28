44500aa4ddb1306b79f239077d9618f9c27b3ea3
Jose Martins <j.msantos.netto@gmail.com>
2023-10-28 00:45:55 -0300

    Initial commit and create window

---
+++ src/main/java/br/com/eclud/Window.java
@@ -0,0 +1,84 @@
+package br.com.eclud;
+
+import org.lwjgl.Version;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.opengl.GL;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
+public class Window {
+    private static Window window = null;
+    private int width, height;
+    private String title;
+    private long glfwWindow;
+
+    private Window() {
+        this.width = 1920;
+        this.height = 1080;
+        this.title = "Eclud engine";
+    }
+
+    public static Window get() {
+        if (window == null) {
+            window = new Window();
+        }
+
+        return window;
+    }
+
+    public void run() {
+        System.out.println("Hello Eclud engine! LWJGL version: " + Version.getVersion() + "!");
+
+        init();
+        loop();
+    }
+
+    public void init() {
+        // Setup error callback
+
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        // Initialize GLFW
+        if (!glfwInit()) {
+            throw new IllegalStateException("Unable to initialize GLFW");
+        }
+
+        // Configure GLFW
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+        glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
+
+        // Create the window
+        glfwWindow = glfwCreateWindow(this.width, this.height, this.title, NULL, NULL);
+        if (glfwWindow == NULL) {
+            throw new IllegalStateException("Failed to create GLFW window");
+        }
+
+        // Make the OpenGL context current
+        glfwMakeContextCurrent(glfwWindow);
+
+        // Enable V-Sync
+        glfwSwapInterval(1);
+
+        // Make a window visible
+        glfwShowWindow(glfwWindow);
+
+        // Ensure to create the bindings
+        GL.createCapabilities();
+    }
+
+    public void loop() {
+        while (!glfwWindowShouldClose(glfwWindow)) {
+            // Poll events
+            glfwPollEvents();
+
+            glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            glfwSwapBuffers(glfwWindow);
+        }
+    }
+}
